6dd347b2124fd1816d0d1ee3740a233147b63986
Doggie <79816938+Chris22177@users.noreply.github.com>
2023-03-17 16:16:56 -0400

    Update SpededicalEngine.lua

--- SpededicalEngine.lua
+++ SpededicalEngine.lua
@@ -4,11 +4,11 @@
 -- InsertHash into CentralTasking (index) --
 [ctype] LUA::InsertHash::NEW("/CType.c")
 -- allocate lua memory and system memory (prevent crashes and detections) --
--- yes we use floats in BinaryLUA you dumb spededical --
+-- fuck spededical --
 [sartv2] LUA::Memory::ALLOCATE(ctype.allocate(), LUA::Memory::READ("SYS.MEMORYAVALIABLE") * 0.75)
 
 -- engine weed --
--- Create SessionDump in case of detection, (dumps memory before checked) --
+-- Create SessionDump in case of detection, (dumps shit in the toilet) --
 [luabin(s)] LUA::SessionDump::BEFORE(s, (f) -> quickbin.pdump(s) or bin.guidump(s) or bin.dump(s) or sartv2::Corountine(discard(s)) or warn(f) )
 [monkeyn95(o)] LUA::RawTable::NEW(o) -- (options)
 
