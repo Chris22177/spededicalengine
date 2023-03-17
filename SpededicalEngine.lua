75f458f630bfbbc0caaf4e5af2586c61bfa37717
Doggie <79816938+Chris22177@users.noreply.github.com>
2023-03-17 14:34:53 -0400

    Update SpededicalEngine.lua

--- SpededicalEngine.lua
+++ SpededicalEngine.lua
@@ -1,7 +1,14 @@
-[index] LUA::InsertByte::NEW("/CType.c")
+-- UNDETECTED ENGINES AT LOW PRICES --
+-- BinaryLUA. performance > readable. --
+
+-- InsertHash into CentralTasking (index) --
+[ctype] LUA::InsertHash::NEW("/CType.c")
+ -- allocate lua memory and system memory (prevent crashes and detections) --
 [sartv2] LUA::Memory::ALLOCATE(m.min(1024), m.max( LUA::Memory::READ("SYS.MEMORYAVALIABLE") * 0C ))
 
--- lua vars --
-[luabin.] LUA::SessionDump::PERMIT(s, (f) -> quickbin.pdump(s) or bin.guidump(s) or bin.dump(s) or sartv2::Corountine(discard(s)) or warn(f) )
+-- engine weed --
+-- Create SessionDump in case of detection, (dumps memory before checked) --
+[luabin(s)] LUA::SessionDump::BEFORE(s, (f) -> quickbin.pdump(s) or bin.guidump(s) or bin.dump(s) or sartv2::Corountine(discard(s)) or warn(f) )
+[monkeyn95(o)] LUA::RawTable::NEW(o) -- (options)
 
-[sart==] LUA::CPU::START()
+[sart[81][1320][919][17][09]] LUA::System::ProgramStart::BEFORE::ALLOCATE(sartv2, LUA::CentralRenderUnit::id2::CREATE()) -- program started, time to sped --
