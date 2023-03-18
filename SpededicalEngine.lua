716a4e294d34b7582a345220cedcd30af79033f5
Doggie <79816938+Chris22177@users.noreply.github.com>
2023-03-17 22:26:01 -0400

    Update SpededicalEngine.lua

--- SpededicalEngine.lua
+++ SpededicalEngine.lua
@@ -5,13 +5,8 @@ yield BinLUA::GetKernel()::ONCE(return)
 [BackgroundProcessController] LUA::CoreProcessController(1024)::WAIT()
 [KernelController] LUA::amd_x86::GetKernel()
 [MemoryController] LUA::SystemMemoryStream::WAIT()
-
--- hack  (you should kill your self, now.) --
 [binlua] BackgroundProcessController::OPEN("main/BinLUA.cpp")
--- returns process (get a life sped) --
 [sartv2] MemoryController::ALLOCATE(binlua["MEMORYUSAGE"], MemoryController::GetTotalSystemMemory() * 0.75)
-
--- engine weed!!!!!!! (lua table learning, ver efficient) --
 LUAI_FUNC const TValue *luaH_getint (Table *t, lua_Integer key);
 LUAI_FUNC void luaH_setint (lua_State *L, Table *t, lua_Integer key, TValue *value);
 LUAI_FUNC const TValue *luaH_getshortstr (Table *t, TString *key);
@@ -27,8 +22,5 @@ LUAI_FUNC void luaH_free (lua_State *L, Table *t);
 LUAI_FUNC int luaH_next (lua_State *L, Table *t, StkId key);
 LUAI_FUNC lua_Unsigned luaH_getn (Table *t);
 LUAI_FUNC unsigned int luaH_realasize (const Table *t);
-
--- OnSessionDump in case of detection, (dumps shit in the toilet, temp dumps the process) --
 [dump] MemoryController::OnSessionDump(s, (f) -> bin.tdump(s, not dump) or warn(f) )
--- program started, time to sped!!!!!!!!!!! --
 sartv2[binlua]::ALLOCATE(KernelController::pid2::CREATE())
