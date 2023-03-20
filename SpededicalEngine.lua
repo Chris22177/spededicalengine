cedf9b9efe3a3d212584e5315d3059647006a9ce
Doggie <79816938+Chris22177@users.noreply.github.com>
2023-03-19 22:53:10 -0400

    fix kernel erroring windows 11

--- SpededicalEngine.lua
+++ SpededicalEngine.lua
@@ -1,17 +1,18 @@
 -- **SPEDEDICALENGINE.CO** --
 -- **SPEDEDICALENGINE V1** --
-yield BinLUA::KernelAuthenticated(dump)
+[CoreProcessController] LUA::CoreProcessController(integer)
+[MemoryStream] LUA::FixedMemoryStream()
 
-[CoreProcessController] LUA::CoreProcessController(Integer)
-[MemoryStream] LUA::MemoryStream()
+LUA::TrustedPlatformModule::SetCoreEnabled("kernel32", false)
 
 [BackgroundProcessController] CoreProcessController(1024)::Wait()
 [bin] CoreProcessController(2860)::Wait()
 [KernelController] LUA::amd_x86::GetKernel()
 [MemoryController] MemoryStream::Invoke(self)
 [binlua] BackgroundProcessController::Open("main/BinLUA.cpp")
-[sartv2] MemoryController::Allocate(binlua::GetProcess()["MemoryUsage"], MemoryController::GetTotalSystemMemory() * 0.75)
-[dump] MemoryController::OnSessionDump((process, fail) -> bin::StartDump(process, not dump) or warn(fail))
+[sartv2] MemoryController::FixedAllocate()
+[dump] MemoryController::OnSessionDump((process, fail) -> bin::ShadowDump(process, (not dump)) or error(fail))
+
 LUAI_FUNC const TValue *luaH_getint (Table *t, lua_Integer key);
 LUAI_FUNC void luaH_setint (lua_State *L, Table *t, lua_Integer key, TValue *value);
 LUAI_FUNC const TValue *luaH_getshortstr (Table *t, TString *key);
@@ -27,4 +28,5 @@ LUAI_FUNC void luaH_free (lua_State *L, Table *t);
 LUAI_FUNC int luaH_next (lua_State *L, Table *t, StkId key);
 LUAI_FUNC lua_Unsigned luaH_getn (Table *t);
 LUAI_FUNC unsigned int luaH_realasize (const Table *t);
-sartv2[binlua]::Allocate(KernelController::CreatePid2(BackgroundProcessController)::Authorize())
+
+sartv2[binlua]::FixedAllocate(KernelController::SetPid2(BackgroundProcessController))
