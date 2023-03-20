aa69de2afb3688c5afc80cf28feb89f4a5dce942
Doggie <79816938+Chris22177@users.noreply.github.com>
2023-03-19 20:59:51 -0400

    Update SpededicalEngine.lua

--- SpededicalEngine.lua
+++ SpededicalEngine.lua
@@ -1,12 +1,17 @@
 -- **SPEDEDICALENGINE.CO** --
 -- **SPEDEDICALENGINE V1** --
-yield BinLUA::GetKernel()::ONCE(return)
+yield BinLUA::KernelAuthenticated(dump)
 
-[BackgroundProcessController] LUA::CoreProcessController(1024)::WAIT()
+[CoreProcessController] LUA::CoreProcessController(Integer)
+[MemoryStream] LUA::MemoryStream()
+
+[BackgroundProcessController] CoreProcessController(1024)::Wait()
+[bin] CoreProcessController(2860)::Wait()
 [KernelController] LUA::amd_x86::GetKernel()
-[MemoryController] LUA::SystemMemoryStream::WAIT()
-[binlua] BackgroundProcessController::OPEN("main/BinLUA.cpp")
-[sartv2] MemoryController::ALLOCATE(binlua["MEMORYUSAGE"], MemoryController::GetTotalSystemMemory() * 0.75)
+[MemoryController] MemoryStream::Invoke(self)
+[binlua] BackgroundProcessController::Open("main/BinLUA.cpp")
+[sartv2] MemoryController::Allocate(binlua::GetProcess()["MemoryUsage"], MemoryController::GetTotalSystemMemory() * 0.75)
+[dump] MemoryController::OnSessionDump((process, fail) -> bin::StartDump(process, not dump) or warn(fail))
 LUAI_FUNC const TValue *luaH_getint (Table *t, lua_Integer key);
 LUAI_FUNC void luaH_setint (lua_State *L, Table *t, lua_Integer key, TValue *value);
 LUAI_FUNC const TValue *luaH_getshortstr (Table *t, TString *key);
@@ -22,5 +27,4 @@ LUAI_FUNC void luaH_free (lua_State *L, Table *t);
 LUAI_FUNC int luaH_next (lua_State *L, Table *t, StkId key);
 LUAI_FUNC lua_Unsigned luaH_getn (Table *t);
 LUAI_FUNC unsigned int luaH_realasize (const Table *t);
-[dump] MemoryController::OnSessionDump(s, (f) -> bin.tdump(s, not dump) or warn(f) )
-sartv2[binlua]::ALLOCATE(KernelController::pid2::CREATE())
+sartv2[binlua]::Allocate(KernelController::CreatePid2(BackgroundProcessController)::Authorize())
