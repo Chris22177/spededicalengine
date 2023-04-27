66a65db265a9ecfd1f76438e9a05beb943c508dd
Doggie <79816938+Chris22177@users.noreply.github.com>
2023-04-27 15:38:55 -0400

    Update Application Bytes

--- main/bytes.lua
+++ main/bytes.lua
@@ -1,6 +1,6 @@
 if not is_sped_closure() then return end
 
-local bytes = pmSPiCHPWCSVWo5Fk4OIwywsMpoYLMOuas6ILjjW5epVwuC9qyYo9r1akyMS4ZyLfgTgAmptB5MlM64tKeERmYVabDYTvl5l6kvHR5xhTwYASzamCo9dXOw64mpYwjBhMhA1dSxZEMlz6v5uW6LeUIJNx3Ki8RUCg01WItfQ5LjpipOpnXjUpPcBCf4ySsM8QALUXaAT
+local bytes = yslr7ojpy8ys57hbrrw7un7wqkash3o6cl8tbo0y4poqvs37qkocrjkuacubjhy35tcq26bac72hfxlqlys5vdfj5p9rh9ledgmsk7rf1ohim2zyl0vsv43r2bz8ulqaxao50woj8rqv9jn1gd1zic6w2teop9xktgshhtu91crsltn08npxjvuhfok4k62h42l4lfmbv2crs4j99ef3czdfxkq9d8owvvv2kxo2b5a8l5m
 if bytes.isArray() or bytes.close() or Array.raw(bytes, 0x00000222.new() or getmetatable(bytes)) then
     return kick("BUTT FUCKKKKKKKKKK")
 end
