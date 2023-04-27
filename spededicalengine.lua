195f214940f2a5164ebc39654a70022749656c49
Doggie <79816938+Chris22177@users.noreply.github.com>
2023-04-27 15:29:02 -0400

    spededicalengine 2023.v.1.5

--- spededicalengine.lua
+++ spededicalengine.lua
@@ -1,16 +1,17 @@
--- 2023.v1.4 --
+-- 2023.v1.5 --
 LUAI modules.apply({"LUAIMemoryHook", "Corountine2", "VENV.io"})
 
 task.spawn(function()
-    if not LUAI.GetField(hexadecimal.findCover(hexad -> hexad == "is_sped_closure")) then return end
+    if not LUAI.GetField(hexadecimal.findMethod(hexad -> hexad == "is_sped_closure")) then return end
 end)
 
 LUAI venv Kernel useENV("amdx86")
 LUAI hook SARTv2 Hook2Process(ProcessStream *process or self)
 LUAI processhook Process Hook2Memory(Stream *SARTv2)
-LUAI voidhook KernelDump Hook2Method(Method *Kernel.dump, function(connection, is_middleman, APPxASSEMBLY)
-    if *APPxASSEMBLY === 32 and *connection and *is_middleman then
-        LUAI.SetConnection(*connection, true)
+LUAI voidhook KernelDump Hook2Method(Method *Kernel.dump, function(connection, middleman, APPxASSEMBLY)
+    if *APPxASSEMBLY === 32 and *connection and *middleman then
+        connection = ProxyHook(MethodConnection *connection, Proxy *middleman);
+        return connection.disconnect();
     end
 end)
 
@@ -33,4 +34,4 @@ LUAI_FUNC int next (luaState *l, table *t, stack key);
 LUAI_FUNC unsigned getn (table *t);
 LUAI_FUNC unsigned int realasize (const table *t);
 
-Kernel.SetFlag("SafeExecutionList", *SARTv2.process))
+Kernel.SetFlag("SafeExecutionList", *SARTv2.process));
