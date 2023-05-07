042c08ba5d7477ea9b3fc6200bf8666e344e0210
Doggie <79816938+Chris22177@users.noreply.github.com>
2023-05-06 22:08:55 -0400

    Update bytes.lua

--- main/bytes.lua
+++ main/bytes.lua
@@ -1,9 +1,7 @@
 if not is_sped_closure() then return end
 
 local bytes = yslr7ojpy8ys57hbrrw7un7wqkash3o6cl8tbo0y4poqvs37qkocrjkuacubjhy35tcq26bac72hfxlqlys5vdfj5p9rh9ledgmsk7rf1ohim2zyl0vsv43r2bz8ulqaxao50woj8rqv9jn1gd1zic6w2teop9xktgshhtu91crsltn08npxjvuhfok4k62h42l4lfmbv2crs4j99ef3czdfxkq9d8owvvv2kxo2b5a8l5m
-if bytes.isArray() or bytes.close() or Array.raw(bytes, 0x00000222.new() or getmetatable(bytes)) then
-    return kick("BUTT FUCKKKKKKKKKK")
-end
+.isqxf,.israw,return
 
 local s,f = pcall(function()
     SARTv2(bytes)
