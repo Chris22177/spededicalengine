e1b2bc9acb0157c3c8240d127a02b882bf8c8277
Doggie <79816938+Chris22177@users.noreply.github.com>
2023-05-08 01:22:20 -0400

    spededicalengine AIAPPLY 2023.v1.7

--- spededicalengine.lua
+++ spededicalengine.lua
@@ -1,32 +1,33 @@
--- 2023.v1.7 --
+-- AIAPPLY 2023.v1.7 --
 LUAI modules.apply({"LUAIMemoryHook", "Corountine2", "VENV.io", "window.luai", "taskbar.luai"})
 
 task.spawn(function()
-    if not .LUAI, .find("is_sped_closure") then return end
+    if not .LUAI, .find("is_sped_closure"), .isqxf, .isbuffer then return end
 end)
 
+LUAI:
+
 LUAI:void Kernel useENV("amdx86")
-LUAI:buffer Certificate Kernel.CreateCertificate()
-LUAI:void SARTv2 Hook2(ProcessStream *process or self)
+LUAI:void Certificate Kernel.CreateCertificate()
+LUAI:void SARTv2 Hook2(Stream *process)
 LUAI:void Process Hook2(Stream *SARTv2)
+
 LUAI:void Middleman Hook2(Method *Kernel.*, function(target, connection, APPx)
-    if not target == APPx then return end
+    if not *target == *APPx then return end
         
     if not *APPx.middleman then
        pcall(function()
-           APPx.middleman = Certificate.openSSH(null, null, ProxyHook:void = self)
+           Certificate.open(ProxyHook:void = Certificate.openProxy).set(APPx)
        end)
     end
         
     local s,f = pcall(function()
-        connection = ProxyHook(MethodConnection *connection, Proxy *APPx.middleman, nil or null or false );
-                
-        return connection.disconnect();
+        ProxyHook(MethodConnection *connection, Proxy or qxf *APPx.middleman, false or nil).connection.close()
     end)
         
     if f then
-       LUAI.kick(string.format("ProxyHook Failed [%s]", f)); 
+       LUAI.idisconnect(string.format("ProxyHook Failed [%s]", f));
     end
 end)
 
-LUAI.setHookLearning(1 or 0x1, { 1: Hook2, 2: APPx }) -- AI Learning Curve, AI apply identifiers --
+LUAI.setHookLearning(1 or 0x1, { 1: Hook2, 2: APPx, 3: ProxyHook:void }) -- AI Learning Curve, AI apply identifiers --
